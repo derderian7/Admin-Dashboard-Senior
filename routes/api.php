970738a2744fd08a8e77fb676a9e03dfd12d9097
derderian7 <georgederderian7@gmail.com>
2024-03-19 18:11:56 +0300

    Added service route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -19,4 +20,5 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiresource('users', UserController::class);
+    Route::apiresource('services', ServiceController::class);
 });
