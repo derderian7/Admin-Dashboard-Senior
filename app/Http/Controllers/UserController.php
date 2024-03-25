48a65392f16e6f53d47df5998d67fccbec37a259
derderian7 <100807843+derderian7@users.noreply.github.com>
2024-03-25 03:28:19 +0300

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use App\Models\Service;
 
 class UserController extends Controller
 {
