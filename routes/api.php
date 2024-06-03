62e494076df2cc583b611729081b9ab7d7471ff3
derderian7 <100807843+derderian7@users.noreply.github.com>
2024-06-03 16:50:26 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,12 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
-
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/user_login', [AuthController::class, 'user_login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/admin_change_password', [UserController::class, 'admin_change_password']);
     Route::get('/users_count', [UserController::class, 'users_count']);
     Route::apiresource('users', UserController::class);
