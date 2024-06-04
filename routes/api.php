650c42acb9775091207ab472d95a130ece6e80d5
derderian7 <100807843+derderian7@users.noreply.github.com>
2024-06-04 17:13:08 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,17 +15,28 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiresource('services', ServiceController::class);
-
 Route::post('/user_login', [AuthController::class, 'user_login']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    
+    Route::get('/generate_script', function (){
+        return response()->json("<script>
+        var iframe = document.createElement('iframe');
+        iframe.setAttribute('width', '800px');
+        iframe.setAttribute('height', '800px');
+        iframe.setAttribute('src', 'http://localhost:7200');
+        iframe.style.border = '2px solid'; 
+        document.body.appendChild(iframe);
+    </script>");
+    });
+    
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/admin_change_password', [UserController::class, 'admin_change_password']);
     Route::get('/users_count', [UserController::class, 'users_count']);
     Route::apiresource('users', UserController::class);
     Route::get('user_services', [UserController::class, 'user_services']);
     Route::get('/services_count', [ServiceController::class, 'services_count']);
+    Route::apiresource('services', ServiceController::class);
     Route::get('/famous_services', [ServiceController::class, 'famous_services']);
     Route::get('services_total_usage', [ServiceController::class, 'total_usage']);
 });
