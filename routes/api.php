13173ec7b9b8125dd51f961c9eca2e4eada9676a
derderian7 <100807843+derderian7@users.noreply.github.com>
2024-06-07 06:04:56 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
